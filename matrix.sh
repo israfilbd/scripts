88194293907e6e823780aa9d64060b4980b7f789
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-07-08 18:31:08 +0600

    Init matrix script

---
+++ matrix.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init --depth=1 -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b matrix https://github.com/ij-israfil/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=ij-israfil
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch lineage_RMX1901-ap2a-user
+echo "============="
+
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+mka bacon
