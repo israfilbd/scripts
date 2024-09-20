b28e1b7072c3a784c797bdb5ffb20bac2b062f9b
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-09-20 11:41:08 +0600

    Init yaap script

---
+++ yaap.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b yaap https://github.com/israfilbd/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=ij-israfil
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch yaap_RMX1901-userdebug
+echo "============="
+
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+m yaap
