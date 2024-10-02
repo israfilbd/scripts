488a8d698cb6772393f8e41652f25b8fdb6d2b22
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-10-02 12:47:04 +0600

    Initial script for afterlife OS

---
+++ afterlife.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b afterlife https://github.com/ij-israfil/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=ij-israfil
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+#build the rom
+. build/envsetup.sh
+m installclean
+goafterlife RMX1901-user
