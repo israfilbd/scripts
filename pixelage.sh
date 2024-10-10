1092604b900d4ad9c1516f5343f9d9b44541ff64
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-10-10 20:10:18 +0600

    Initial pixelage script

---
+++ pixelage.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init -u --depth=1 https://github.com/ProjectPixelage/android_manifest.git -b 15 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b pixelage https://github.com/israfilbd/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=ij-israfil
+export BUILD_HOSTNAME=crave
+export PIXELAGE_BUILD=RMX1901
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch pixelage_RMX1901-ap3a-user
+echo "============="
+
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+make bacon
