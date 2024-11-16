733a330e268a692f5e7d44ff13cab29b19e5ef12
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-11-16 13:36:29 +0600

    Create evox-vic.sh
    initial evox A15 script

---
+++ evox-vic.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# Rom source repo
+repo init --depth=1 -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Clone local_manifests repository
+git clone -b cr-15 https://github.com/israfilbd/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=ij-israfil
+export BUILD_HOSTNAME=crave
+export TZ=Asia/Dhaka
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch lineage_RMX1901-ap3a-user
+echo "============="
+
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+m evolution
