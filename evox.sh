2beaf3557b2d89c05346539f1a198a9c903f642a
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-12-24 00:23:38 +0600

    Update launch command to ap4a

--- evox.sh
+++ evox.sh
@@ -29,7 +29,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_RMX1901-ap3a-user
+lunch lineage_RMX1901-ap4a-user
 echo "============="
 
 # Make cleaninstall
