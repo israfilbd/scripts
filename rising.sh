bbcb03cfd7ace7f3cc192eb5a72a121959e61223
israfilbd <144210295+israfilbd@users.noreply.github.com>
2024-12-29 23:56:44 +0600

    Update rising.sh
    Use my local timezone Asia/Dhaka

--- rising.sh
+++ rising.sh
@@ -2,6 +2,10 @@
 
 rm -rf .repo/local_manifests/
 
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
+
 # Rom source repo
 repo init --depth=1 -u https://github.com/RisingOS-staging/android -b fifteen --git-lfs
 echo "=================="
@@ -21,7 +25,6 @@ echo "============================"
 # Export
 export BUILD_USERNAME=ij-israfil
 export BUILD_HOSTNAME=crave
-export TZ="Asia/Dhaka"
 echo "======= Export Done ======"
 
 # Set up build environment
