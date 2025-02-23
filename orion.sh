d01fb88455bfff830410eefddb96cd970fad54dc
israfilbd <144210295+israfilbd@users.noreply.github.com>
2025-02-23 21:51:00 +0600

    Initial script for Orion A15

--- orion.sh
+++ orion.sh
@@ -2,14 +2,18 @@
 
 rm -rf .repo/local_manifests/
 
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
+
 # Rom source repo
-repo init --depth=1 -u https://github.com/OrionOS-prjkt/android -b 14.0 --git-lfs
+repo init --depth=1 -u https://github.com/OrionOS-Project/manifest -b vic --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone -b orion-14 https://github.com/ij-israfil/local_manifests .repo/local_manifests
+git clone -b abcd https://github.com/israfilbd/local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -18,10 +22,17 @@ echo "============================"
 /opt/crave/resync.sh
 echo "============================"
 
+# Mic Fixes
+cd frameworks/base/services/core/java/com/android/server/policy && rm -rf PhoneWindowManager.java
+wget -r https://raw.githubusercontent.com/israfilbd/fixes/refs/heads/main/abcd/PhoneWindowManager.java
+cd -
+echo "========================"
+echo "Microphone Fix successful"
+echo "========================"
+
 # Export
 export BUILD_USERNAME=ij-israfil
 export BUILD_HOSTNAME=crave
-export TZ=Asia/Dhaka
 echo "======= Export Done ======"
 
 # Set up build environment
@@ -29,7 +40,7 @@ source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch orion_RMX1901-ap2a-user
+lunch orion_RMX1901-ap4a-user
 echo "============="
 
 # Make cleaninstall
@@ -37,4 +48,4 @@ make installclean
 echo "============="
 
 # Build rom
-mka space
+make orion
