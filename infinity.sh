5747811e279eca6449c6f0f644dedef423ff8c37
israfilbd <144210295+israfilbd@users.noreply.github.com>
2025-07-15 13:11:26 +0600

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -7,13 +7,13 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
 
 # Rom source repo
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone -b infinity-15 https://github.com/israfilbd/local_manifests .repo/local_manifests
+git clone -b infinity-16 https://github.com/israfilbd/local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -40,4 +40,4 @@ make installclean
 echo "============="
 
 # Build rom
-mka bacon
+m bacon
