52f7829035875809b13d4d6379a24b2c3feadad2
israfilbd <144210295+israfilbd@users.noreply.github.com>
2025-11-07 08:29:00 +0600

    Bring RisingOS-Revived
    Added UDFPS target export and applied DT2W patch if not present.

--- rising.sh
+++ rising.sh
@@ -7,13 +7,13 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
 
 # Rom source repo
-repo init --depth=1 -u https://github.com/RisingOS-staging/android -b fifteen --git-lfs
+repo init --depth=1 -u https://github.com/RisingOS-Revived/android -b sixteen --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone -b rising-15 https://github.com/israfilbd/local_manifests .repo/local_manifests
+git clone -b rising-16 https://github.com/israfilbd/local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -25,14 +25,18 @@ echo "============================"
 # Export
 export BUILD_USERNAME=ij-israfil
 export BUILD_HOSTNAME=crave
+export TARGET_HAS_UDFPS=true
 echo "======= Export Done ======"
+
+# Mic Fix
+echo ">>> Applying frameworks/base patch..." && cd frameworks/base && (git log --oneline | grep -q "dt2w\|DT2W\|double.*tap" || (wget -O temp.patch "https://github.com/ij-project/frameworks_base_evox/commit/c49d293.patch" && (git apply temp.patch && git add . && git commit -m "Apply DT2W patch" || echo "Patch conflicts detected, continuing build...") && rm -f temp.patch)) && cd ../.. && echo ">>> Frameworks/base patch process completed!"
 
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_RMX1901-ap4a-user
+riseup RMX1901 user
 echo "============="
 
 # Build rom
