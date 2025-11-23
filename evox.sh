4bc0426ca5b384ce44236ab1b4685f84ab19baec
israfilbd <144210295+israfilbd@users.noreply.github.com>
2025-11-23 13:55:55 +0600

    Add BUILD_BROKEN_DUP_RULES environment variable

--- evox.sh
+++ evox.sh
@@ -25,6 +25,8 @@ echo "============================"
 # Export
 export BUILD_USERNAME=ij-israfil
 export BUILD_HOSTNAME=crave
+export BUILD_BROKEN_DUP_RULES=true
+
 echo "======= Export Done ======"
 
 # Set up build environment
