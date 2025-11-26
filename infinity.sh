9b5dc3e3c92869741c8d4fd39dd22d6a92ac1f5f
israfilbd <144210295+israfilbd@users.noreply.github.com>
2025-11-26 13:55:44 +0600

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -25,6 +25,7 @@ echo "============================"
 # Export
 export BUILD_USERNAME=ij-israfil
 export BUILD_HOSTNAME=crave
+export TARGET_HAS_UDFPS=true
 echo "======= Export Done ======"
 
 # Mic Fix
