adc10d433966b6e78ae57f114d5405b974e52fd3
israfilbd <144210295+israfilbd@users.noreply.github.com>
2025-11-28 11:18:46 +0600

    Update clover.sh

--- clover.sh
+++ clover.sh
@@ -1,50 +1,12 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests/
-
-# Local TimeZone
-sudo rm -rf /etc/localtime
-sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
-
-# Rom source repo
-repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr1 --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
-
-# Clone local_manifests repository
-git clone -b clover https://github.com/israfilbd/local_manifests .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
-
-# Sync the repositories
-/opt/crave/resync.sh
-echo "============================"
-
-# Remove Project
-# rm -rf hardware/interfaces/sensors/2.0/multihal
-echo "======= Remove Done ======"
-
-# Export
-export BUILD_USERNAME=ij-israfil
-export BUILD_HOSTNAME=crave
-export WITH_GMS=true
-export TARGET_DISABLE_EPPE=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-echo "======= Export Done ======"
-
-# Set up build environment
-source build/envsetup.sh
-echo "====== Envsetup Done ======="
-
-# Lunch
-lunch clover_RMX1901-bp3a-userdebug
-echo "============="
-
-# Make cleaninstall
-make installclean
-echo "============="
-
-# Build rom
+rm -rf .repo/local_manifests && \
+repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr1 --git-lfs && \
+git clone https://github.com/israfilbd/local_manifests --depth 1 -b clover .repo/local_manifests && \
+/opt/crave/resync.sh && \
+export BUILD_USERNAME=ij-israfil ; \
+export BUILD_HOSTNAME=crave ; \
+source build/envsetup.sh && \
+lunch clover_RMX1901-bp3a-userdebug && \
+make installclean && \
 mka clover
