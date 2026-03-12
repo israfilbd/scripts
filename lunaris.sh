72920ac51708cba4e3f11a19afd877b98b0bb32a
israfilbd <144210295+israfilbd@users.noreply.github.com>
2026-03-12 13:33:17 +0000

    lunaris: go for A16 QPR2

--- lunaris.sh
+++ lunaris.sh
@@ -7,7 +7,7 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
 
 # Rom source repo
-repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs
+repo init --depth=1 -u https://github.com/Lunaris-AOSP/android -b 16.2 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
@@ -22,23 +22,23 @@ echo "============================"
 /opt/crave/resync.sh
 echo "============================"
 
+# Remove Project
+rm -rf hardware/interfaces/sensors/2.0/multihal
+echo "======= Remove Done ======"
+
 # Export
 export WITH_GMS=true
-export TARGET_USES_OMNI_GAPPS=true
-export TARGET_SUPPORTS_GOOGLE_FILES=true
+export TARGET_CUSTOM_UDFPS= true
 export BUILD_USERNAME=ij-israfil
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
-
-# Mic Fix
-echo ">>> Applying frameworks/base patch..." && cd frameworks/base && (git log --oneline | grep -q "dt2w\|DT2W\|double.*tap" || (wget -O temp.patch "https://github.com/ij-project/frameworks_base_evox/commit/c49d293.patch" && (git apply temp.patch && git add . && git commit -m "Apply DT2W patch" || echo "Patch conflicts detected, continuing build...") && rm -f temp.patch)) && cd ../.. && echo ">>> Frameworks/base patch process completed!"
 
 # Set up build environment
 source b*/env*
 echo "====== Envsetup Done ======="
 
 # Lunch
-lunch lineage_RMX1901-bp2a-user
+lunch lineage_RMX1901-bp4a-user
 echo "============="
 
 # Make cleaninstall
@@ -46,4 +46,4 @@ make installclean
 echo "============="
 
 # Build rom
-m lunaris
+m bacon
