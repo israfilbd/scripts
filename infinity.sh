2ff04b77b522b15fd9bdd7f06c6fbcb6392b49cc
israfilbd <144210295+israfilbd@users.noreply.github.com>
2026-08-22 20:46:23 +0600

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -7,13 +7,13 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/Dhaka /etc/localtime
 
 # Rom source repo
-repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify --git-lfs -u https://github.com/ij-project/infinity-manifest -b 16 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone -b infinity-16.2 https://github.com/israfilbd/local_manifests .repo/local_manifests
+git clone -b infinity-16.2.v2 https://github.com/israfilbd/local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -26,14 +26,10 @@ echo "============================"
 rm -rf hardware/interfaces/sensors/2.0/multihal
 echo "======= Remove Done ======"
 
-# UDFPS & Mic Fix
-echo ">>> Cherry picking..." && cd frameworks/base && git fetch https://github.com/ij-project/frameworks_base && (git cherry-pick e98cb38 || git cherry-pick --skip) && cd ../..
-echo "======= Cherry picking Done ======"
-
 # Export
 export BUILD_USERNAME=ij-israfil
 export BUILD_HOSTNAME=crave
-export TARGET_HAS_UDFPS=true
+export TARGET_ENABLE_BLUR=true
 echo "======= Export Done ======"
 
 # Set up build environment
